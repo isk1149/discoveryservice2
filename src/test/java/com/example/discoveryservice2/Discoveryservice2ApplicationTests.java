403086cb3322019713c5e97716f1e4306156e403
Inseong Kim <isk1149@naver.com>
2024-01-23 06:21:44 +0900

    upload

---
+++ src/test/java/com/example/discoveryservice2/Discoveryservice2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.discoveryservice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Discoveryservice2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
